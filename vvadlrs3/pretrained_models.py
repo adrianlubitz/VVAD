aa4f5e6112aacc753c1bb8c5c397fdba7c90866f
Adrian Lubitz <adrianlubitz@gmail.com>
2021-03-31 12:58:52 +0200

    made path relative

--- vvadlrs3/pretrained_models.py
+++ vvadlrs3/pretrained_models.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c037edcd6775169a9416c9a4b3423cb7f6cbd9cc88caef027786d5ae3b6dec54
-size 724
+oid sha256:3f378223f46bfac2780126cb283fe3206f65059f72b4687b112e5ced830a25f5
+size 839
