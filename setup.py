62af973578bf387396f4c792a7f83f175e26c5fc
Adrian Lubitz <adrianlubitz@gmail.com>
2021-03-31 12:23:13 +0200

    models and structure changes

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93a26145b06c8d797fe1d9d8e1506d7f940a11841c0a9917f808e8588eba2084
-size 1928
+oid sha256:d7cbcb1429ff14a86e7d71cf96cfb1acab522f602f9a2b4b38638e7275130b1c
+size 1920
--- vvadlrs3/multiprocessingUtils.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9a4d473c94dd8a721bfa073080b132c23a50251425f626c167133a1e80efc6b8
-size 3035
