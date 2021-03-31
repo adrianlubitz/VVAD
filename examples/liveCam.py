1c5fd78c145f35c1e99c457aa5b37fba2d85aeb7
Adrian Lubitz <adrianlubitz@gmail.com>
2021-03-31 12:49:37 +0200

    started liveCam

--- examples/liveCam.py
+++ examples/liveCam.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2c87eab50e7fdf6ed32d1de3488df3f870161924144d9f3d552452324859bb1
-size 191
+oid sha256:7d07048c8ead4971997dd4cbe3aba90d5b3ad46bd7822a4cb8b3b500bf43bf39
+size 503
