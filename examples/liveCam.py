daa8b6122486f5448be7ea3d1b79f24fd66afd0a
Adrian Lubitz <adrianlubitz@gmail.com>
2021-04-08 19:49:41 +0200

    some tries with the live demo...but until now nothing is really working

--- examples/liveCam.py
+++ examples/liveCam.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19a10451ed95e8e4c31c9c795d80f6aa2a57a68aa274a5b8cb3821e12bdc246c
-size 819
+oid sha256:d7b8a6dcc83c6d453949ff4ad8e0c7a061c632296a04ad5a8a5421a76dc69408
+size 2938
