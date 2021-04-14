cf109907bf0fa71e55a374a8fcf4c0138cb1551c
Adrian Lubitz <adrianlubitz@gmail.com>
2021-04-14 14:16:28 +0200

    starting experiments

--- vvadlrs3/utils/videoUtils.py
+++ vvadlrs3/utils/videoUtils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4c80fa285153e6cff673716f2907aba11a803e1ac6f0e17da2abdb05f6ca67c
-size 598
+oid sha256:136e0481b543f4300d6d940df7363cb534e734d696baf8aafd5a6e53ee6873e4
+size 4033
