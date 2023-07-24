8317a6caa219311866102191f15875542a37280f
K-Zanaty <88384230+K-Zanaty@users.noreply.github.com>
2023-07-24 15:42:17 +0200

    add unit test work directory

--- vvadlrs3/dataSet.py
+++ vvadlrs3/dataSet.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2accb03a3df211d4967eb47a7ae5beb648d22dac4075a754ab6e54abb75a20b
-size 57413
+oid sha256:805a88094ef85a5256eb8ac1ad4f84d12591aa008de99883ee40968bac5e9a3a
+size 57389
