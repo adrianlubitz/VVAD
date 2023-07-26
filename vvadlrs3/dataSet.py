5db9f6fa4008b11650bb88e8ec56c0446da47f3c
K-Zanaty <88384230+K-Zanaty@users.noreply.github.com>
2023-07-26 09:26:48 +0200

    Fixed wrong path and unexpected success

--- vvadlrs3/dataSet.py
+++ vvadlrs3/dataSet.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:805a88094ef85a5256eb8ac1ad4f84d12591aa008de99883ee40968bac5e9a3a
-size 57389
+oid sha256:243b0d01bd52d4410ea2bc0bc9df2fbc9a7bba33b5dc4e9e62d04b11bc4735f3
+size 57266
