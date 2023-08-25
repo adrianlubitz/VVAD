fc80952ea50fb1e0974d74e4dab1419b2454dbae
K-Zanaty <88384230+K-Zanaty@users.noreply.github.com>
2023-08-25 15:35:43 +0200

    first finish of dataset test cases

--- vvadlrs3/dataSet.py
+++ vvadlrs3/dataSet.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:566219b2ca024a38f1e48a1a96130be2cb66d7550ca1ce76857ad57e9afb423b
-size 50322
+oid sha256:480a79ca16787822b0d5a8dc09e6298ed98e98bdf8efed11f5565bd092301dd0
+size 50490
