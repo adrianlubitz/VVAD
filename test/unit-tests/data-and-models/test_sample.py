66d909d149f6c9185905919fe56d272aae540b72
K-Zanaty <88384230+K-Zanaty@users.noreply.github.com>
2023-08-27 20:49:17 +0200

    fix some sample test cases

--- test/unit-tests/data-and-models/test_sample.py
+++ test/unit-tests/data-and-models/test_sample.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cbc11ee5b5c2f2d8b4de470e0f23a51ca2bb59d04db625551b94a7fb00b5d80
-size 7724
+oid sha256:580e3d7fc27a5e7c1a6cef838dda42ed4d8ca32ab76b2c3386ba248830e58fbf
+size 7930
