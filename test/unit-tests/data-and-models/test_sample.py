9d0aedf49bb588711a7295a370d19e918bc7ae69
K-Zanaty <88384230+K-Zanaty@users.noreply.github.com>
2023-08-29 23:47:47 +0200

    Git coverage report only on pull request (does not work with push)

--- test/unit-tests/data-and-models/test_sample.py
+++ test/unit-tests/data-and-models/test_sample.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3948bdf1c5a0328ec98b16c21f4ac9f3b730548c98fa91bcf183df9a98246ece
-size 7495
+oid sha256:d5b0d690d83ba5094e159ae61e511b51221769bec3631f71ebe6fa5635e10ed7
+size 7765
--- vvadlrs3/sample.py
+++ vvadlrs3/sample.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64c7ffe5816ce14347c6be1b994beb9d7bc49140fe740b8ca08a98ffdf5af7a4
-size 20152
+oid sha256:ef74c82ff2f5be17a44a43f5ae9f2646799d474aebece712c32fbdc26fb48ef7
+size 20147
