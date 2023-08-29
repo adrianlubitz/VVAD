a54850c2ae215492d866af17afd12bcf4a3b0698
K-Zanaty <88384230+K-Zanaty@users.noreply.github.com>
2023-08-29 22:47:55 +0200

    Had unexpected successes. So remove the expected failure

--- test/unit-tests/data-and-models/test_sample.py
+++ test/unit-tests/data-and-models/test_sample.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ed80f911e0852465b38ba65e4c55f74095242425517d404d7a36fc83c1717f2
-size 7605
+oid sha256:3948bdf1c5a0328ec98b16c21f4ac9f3b730548c98fa91bcf183df9a98246ece
+size 7495
